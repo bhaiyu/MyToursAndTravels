056166c56c43699d5b07ba787a27c8016c955d53
admin <admin@LENOVO>
2018-10-08 00:53:28 +0530

    Spring boot file..

--- MyToursAndTravels/src/main/java/com/mytoursntravels/Application.java
+++ MyToursAndTravels/src/main/java/com/mytoursntravels/Application.java
@@ -1,7 +1,14 @@
 package com.mytoursntravels;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Spring
+@SpringBootApplication
 public class Application {
 
-}
+	public static void main(String args[]) {
+		  SpringApplication.run(Application.class, args);  
+		  System.out.println("Spring boot apllication");
+    }  
+	}
+
